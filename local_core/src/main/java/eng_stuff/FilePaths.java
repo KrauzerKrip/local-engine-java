e20ec68b1b43e8c3fe674aad22946d946f0247f5
KrauzerKrip <krauzerkrip@gmail.com>
2022-01-29 15:40:42 +0200

    feat(resources): add resources

--- local_core/src/main/java/eng_stuff/FilePaths.java
+++ local_core/src/main/java/eng_stuff/FilePaths.java
@@ -1,28 +1,33 @@
 package eng_stuff;
 
+import java.net.URL;
+
+import game.MainGame;
+
 public final class FilePaths {
-	private final static String PARAMETERS_PATH = //"parameters/"; 
-	private final static String OBJECTS_PATH = "objects/";
-	private final static String SCENES_PATH = "scenes/";
+	
+	private final static URL PARAMETERS_PATH = MainGame.class.getResource("parameters/"); 
+	private final static URL OBJECTS_PATH = MainGame.class.getResource("objects/");
+	private final static URL SCENES_PATH = MainGame.class.getResource("scenes/");
 
 	/**
 	 * @return the PARAMETERS_PATH.
 	 */
-	public final static String getParametersPath() {
+	public final static URL getParametersPath() {
 		return PARAMETERS_PATH;
 	}
 
 	/**
 	 * @return the OBJECTS_PATH
 	 */
-	public final static String getObjectsPath() {
+	public final static URL getObjectsPath() {
 		return OBJECTS_PATH;
 	}
 
 	/**
 	 * @return the SCENES_PATH
 	 */
-	public final static String getScenesPath() {
+	public final static URL getScenesPath() {
 		return SCENES_PATH;
 	}
 }
